cea2f9b7ea3b9aa3b2886e3c6badb3a23473b7f7
yannlelorier <yannllb97@gmail.com>
2022-05-18 10:50:34 -0500

    comments, tests

--- id3.py
+++ id3.py
@@ -1,8 +1,20 @@
+"""
+ID3 Decision Tree contructor
+Takes in a csv file with the training data.
+
+Carla Pérez-Gavilán
+Jorge Palacios Hristova
+Marcela Fuentes Lecuona
+Yann Le Lorier Bárcena
+"""
 import pandas as pd
 import math
 import pprint 
 
 def total_entropy(ds, label):
+    """
+    Function to calculate total (initial) entropy of a system
+    """
     total_entropy = 0 
     total = ds.shape[0]
     target_var = ds[label]
@@ -14,6 +26,9 @@ def total_entropy(ds, label):
     return total_entropy
 
 def entropy_feature(ds_feature, label):
+    """
+    Function to calculate entropy for a single attribute
+    """
     all_var = ds_feature[label]
     total = ds_feature.shape[0]
     result = 0 
@@ -26,6 +41,10 @@ def entropy_feature(ds_feature, label):
     return result
 
 def info_gain(ds, feature_name, label):
+    """
+    Gain information obtained from the total entropy and the entropy for a single attribute
+
+    """
     info_gain = 0 
     unique_features = ds[feature_name].unique()
     class_data = ds[label].unique()
@@ -37,6 +56,9 @@ def info_gain(ds, feature_name, label):
     return total_entropy(ds, label) - info_gain
 
 def max_info_gain(ds, label):
+    """
+    Getting the maximum gain from different attibutes to choose the one that give the most information
+    """
     feature_list = ds.columns.drop(label)
     max = -1 
     max_feature = None
@@ -50,6 +72,9 @@ def max_info_gain(ds, label):
 
 
 def generate_child(feature_name, ds, label):
+    """
+    The function that generates a child after an attribute
+    """
     dict_value_count =  ds[feature_name].value_counts(sort=False)
     subtree = {}
     class_data = ds[label].unique()
@@ -67,40 +92,58 @@ def generate_child(feature_name, ds, label):
     return subtree, ds
 
 def construct_tree(root, prev, ds, label):
+    """
+    The actual construction of the tree
+    """
     class_data = ds[label].unique()
     if ds.shape[0] != 0:
         max_info = max_info_gain(ds, label)
         tree, ds = generate_child(max_info, ds, label)
-        next = None
+        next_ = None
 
         if prev != None: 
             root[prev] = dict()
             root[prev][max_info] = tree
-            next =  root[prev][max_info]
+            next_ =  root[prev][max_info]
         else: 
             root[max_info] = tree
-            next = root[max_info]
+            next_ = root[max_info]
 
-        for node, branch in list(next.items()):
+        for node, branch in list(next_.items()):
             if branch == "unassigned":
                 new_data = ds[ds[max_info] == node]
-                construct_tree(next, node, new_data, label)
+                construct_tree(next_, node, new_data, label)
 
 def id3(ds, label):
+    """
+    Main function call
+    """
     ds_copy = ds.copy()
     tree = {}
     construct_tree(tree, None, ds_copy, label)
     return tree
 
 def print_pretty(d, indent=0):
-   for key, value in d.items():
-      print('  ' * indent + str(key))
-      if isinstance(value, dict):
-         print_pretty(value, indent+1)
-      else:
-         print('  ' * (indent+1) + str(value))
+    """
+    Prints the resulting decision tree in a human-readable way
+    """
+    for key, value in d.items():
+        print('  ' * indent + str(key))
+        if isinstance(value, dict):
+            print_pretty(value, indent+1)
+        else:
+            print('  ' * (indent+1) + str(value))
 
 if __name__ == "__main__":
+    #tenis data
     df = pd.read_csv("tenis.csv")
+
+    #restaurant data
+    #df = pd.read_csv("./../weka/weka-3-8-6-azul-zulu-linux/weka-3-8-6/data/restaurant-data.arff.csv")
+
+    # for tenis data:
     tree = id3(df, 'play')
+    #for restaurant data:
+    #tree = id3(df, 'class')
+
     print_pretty(tree)
